43113d123b06bffdd550ebabb76b66c07986f79e
namily0 <kurimoko28@gmail.com>
2023-09-05 15:50:36 +0900

    [fix]ML-Agentsに関する環境を構築。AddAgentブランチで追加されている予定。

---
+++ Assets/_namily0_workSpace/Scripts/AgentsScripts/Agents.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class Agents : Agent
+{
+    void Start()
+    {
+        
+    }
+
+    public override void OnEpisodeBegin()
+    {
+
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+
+    }
+    
+}
